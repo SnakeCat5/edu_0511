fe841affcbae940f26cad3595a69fb6a57220a17
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-04-16 18:49:21 +0300

    Update Main.java

--- task_1.5/Main.java
+++ task_1.5/Main.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-        public class Main {
-public static void main (String[] args) {
-Scanner scan = new Scanner(System.in);
-int TC = scan.nextInt();
-double TF = 1.8 * TC + 32;
-    System.out.println(TF);
+public class Main {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int TC = scan.nextInt();
+        double TF = 1.8 * TC + 32;
+        System.out.println(TF+ " фарингейд");
     }
 }
