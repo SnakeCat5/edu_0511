3e24d300932112d6114fc7af93e145cea37c03ba
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-04-16 16:19:35 +0300

    Update Main.java

--- task_1.3/Main.java
+++ task_1.3/Main.java
@@ -1,8 +1,7 @@
-/* Объяви переменные a, b типа int и переменную s типа String.
-Сразу же в строке объявления присвой им любые разные значения. */
-
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int a = 5;
+        int b = 2;
+        String s = "Иванов"
     }
 }
