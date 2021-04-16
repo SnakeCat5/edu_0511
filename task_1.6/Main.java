1d168f95f7f841ca0c30c802fa35cebfd0d579a3
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-04-16 18:31:05 +0300

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -1,9 +1,9 @@
-/*
-Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
-*/
-
-public class Main {
-    public static void main(String[] args) {
-        // Ваш код
-    }
+import java.util.Scanner;
+        public class Main {
+public static void main (String[] args) {
+Scanner scan = new Scanner(System.in);
+int a = scan.nextInt();
+double b = a + (Double)Math.floor(a*15)/100.0;
+System.out.println(b);
+}
 }
