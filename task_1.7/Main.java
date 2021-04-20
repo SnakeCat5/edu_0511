ecd3380682af536534147b52bbf4a61eb432ee88
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-04-20 18:17:34 +0300

    Update Main.java

--- task_1.7/Main.java
+++ task_1.7/Main.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-        public class Main {
-public static void main (String[] args) {
-Scanner scan = new Scanner(System.in);
-int a = scan.nextInt();
-int b = scan.nextInt();
-int c = scan.nextInt();
-double d = a*b*c;
-System.out.println("нужно " +d+ " литров воды");
-}
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = a * b * c;
+        System.out.println("нужно " + d + "000 литров воды");
+    }
 }
