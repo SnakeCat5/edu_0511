052de32d4fb5af2d84354d4bc2f3826bb376c8a0
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-04-20 19:20:15 +0300

    Update Main.java

--- task_2.2/Main.java
+++ task_2.2/Main.java
@@ -1,12 +1,11 @@
-/*Выведи на экран таблицу умножения 10 на 10 в следующем виде:
-1 2 3 4 ...
-2 4 6 8 ...
-3 6 9 12 ...
-4 8 12 16 ...
-... */
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (int a = 1; a < 10; a++) {
+            for (int b = 1; b < 10; b++) {
+                System.out.print(a * b + " ");
+            }
+               System.out.println(" ");
 
-    }
-}
+                        }
+                    }
+                }
