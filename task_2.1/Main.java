24e21bfe552f5b7739ba56087c1cc5c5cd4c1b71
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-04-20 16:19:32 +0300

    Update Main.java

--- task_2.1/Main.java
+++ task_2.1/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 String words []= {"Мама","Мыла","Раму"};
-        for (int мама=0;мама<3;мама++)
-        for (int мыла=0;мыла<3;мыла++)
-        if (мама!=мыла)
-        System.out.println(words[мама]+" "+words[мыла]+" "+words[3-мама-мыла]);
+        for (int Мама=0;Мама<3;Мама++)
+        for (int Мыла=0;Мыла<3;Мыла++)
+        if (Мама!=Мыла)
+        System.out.println(words[Мама]+" "+words[Мыла]+" "+words[3-Мама-Мыла]);
     }
 }
