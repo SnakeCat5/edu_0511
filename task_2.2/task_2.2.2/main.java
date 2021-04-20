70e7143ce232e59e91e854241c4dc62201eba1cd
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-04-20 19:59:56 +0300

    Create main.java

---
+++ task_2.2/task_2.2.2/main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int i,j;
+        System.out.printf(" * |");
+        for(i=1; i<10; i++)
+            System.out.printf("%2d ", i);
+        System.out.printf("\n---|------------");
+        System.out.printf("----------------\n");
+        for(i=1; i<10; i++)
+        {
+            System.out.printf("%2d |", i);
+            for(j=1; j<10; j++)
+                System.out.printf("%2d ", i*j);
+            System.out.printf("\n");
+        }
+
+    }
+
+}
