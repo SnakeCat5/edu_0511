ff79a39c1f05ff7ee41bb7c7a86430f87905df89
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-04-22 20:48:20 +0300

    Update Main.java

--- task_2.9/Main.java
+++ task_2.9/Main.java
@@ -1,10 +1,22 @@
-/*
-Задание: Дан массив с числами. Проверьте, что в этом массиве есть число 5. Если есть - выведите 'да', а если нет - выведите 'нет'.
-*/
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        {
+            int[] a = new int[5];
+            int i = 0;
+            Scanner in = new Scanner(System.in);
+            String times[] = {"первый ", "второй ", "третий ", "четвертый ", "пятый "};
+            for (int j = 0; j < 5; j++) {
+                 {
+                    System.out.print("Введите " + (times[j]) + "элемент массива: ");
+                    a[i] = in.nextInt();
+                    if (a[i] == 5) {
+                        System.out.println("Вы ввели правильное число это число: "+a[i]);
+                    } else {
+                        System.out.println("Нужное число не введено вы ввели: "+a[i]);
+                    }
+                }
+            }
+        }
     }
 }
