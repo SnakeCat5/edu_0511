3be7a862e200e2d16d0afd615a036341946deb54
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-04-22 09:59:16 +0300

    Update Main.java

--- task_2.5/Main.java
+++ task_2.5/Main.java
@@ -1,10 +1,13 @@
-/*
-Задание: Запросить у пользователя ввод числа и сохранить это число в переменную a.Если переменная a равна 10, то выведите 'Верно', иначе выведите 'Неверно'.
-*/
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int number = scan.nextInt();
+        if (number == 10) {
+            System.out.println("Это верное число");
+        } else {
+            System.out.println("Это не верное число");
+        }
     }
 }
