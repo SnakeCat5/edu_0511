e4ec3bfde7ef5bf23cbbcbb403eff45d700535ba
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-04-23 16:46:15 +0300

    Update Main.java

--- task_2.12/Main.java
+++ task_2.12/Main.java
@@ -1,16 +1,26 @@
-/*
-ВНИМАНИЕ эта задача для ДЗ и не является обязательной.
-Задание: 1. Создай массив чисел.
-2. Добавь в массив 10 чисел с клавиатуры.
-3. Вывести на экран длину самой длинной последовательности повторяющихся чисел в списке.
-
-Пример для списка 2, 4, 4, 4, 8, 8, 4, 12, 12, 14:
-3
-*/
-
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        //напишите тут ваш код
-
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        String порядковый[] = {"второй", "третий", "четвертый", "пятый", "шестой", "седьмой", "восьмой", "девятый", "десятый"};
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            arr.add(scan.nextInt());
+                System.out.println("Введите " + порядковый[i] + " элемент массива: ");
+            int current = 1, max = 0;
+            for (i = 1; i < arr.size(); i++) {
+                if (arr.get(i).equals(arr.get(i - 1))) {
+                    current++;
+                    if (current > max) {
+                       max = current; System.out.println("Наибольшее колличество последовательных чисел: " + max);
+                    }
+                } else {
+                    current = 1;
+                }
+            }
+        }
     }
 }
