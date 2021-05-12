408bfa8f1406c1673d67dba561d1a6bcebc59184
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-05-12 18:06:34 +0300

    Update Main.java

--- task_3.5/Main.java
+++ task_3.5/Main.java
@@ -1,10 +1,11 @@
-/*
-Задание: Дан массив с числами. Выведите последовательно его элементы используя рекурсию и не используя цикл.
-*/
-
-
 public class Main {
     public static void main(String[] args) {
-        int num[] = {4,2,6,2,65}
-    }
-}
+        int num[] = {4,2,6,2,65};
+         recursion(0, num);
+         }
+public static void recursion (int n, int[] ar){
+        System.out.println(ar[n]);
+        if(n < (ar.length-1)) {
+        recursion(n+1, ar);
+        }
+        }}
