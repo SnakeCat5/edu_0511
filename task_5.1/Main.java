664d12cb1237f21c2bf5100eceb3b138f5dbead9
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-05-21 11:38:13 +0300

    Update Main.java

--- task_5.1/Main.java
+++ task_5.1/Main.java
@@ -5,17 +5,31 @@
 * */
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> names = new ArrayList();
+        ArrayList<String> test = new ArrayList();
         names.add("Анатолий");
         names.add("Георгий");
         names.add("Руслан");
         names.add("Георгий");
         names.add("Павел");
         names.add("Руслан");
+
+        Iterator выборка = names.iterator();
+        Object имя = "";
+        while (выборка.hasNext()) {
+            имя = выборка.next();
+            if(test.contains(имя)){
+                выборка.remove();
+            }else {
+                test.add(имя.toString());
+            }
+        }
+        System.out.println(names);
     }
 }
