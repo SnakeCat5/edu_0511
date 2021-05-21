7f243f40ea2e5e7b9cb1d167337df4ac913cb901
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-05-21 11:48:28 +0300

    Update Main.java

--- task_5.2/Main.java
+++ task_5.2/Main.java
@@ -13,34 +13,33 @@
 Абрамовичи */
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-/* 
-Модернизация ПО
-*/
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        List<String> list = new ArrayList<>();
-        while (true) {
-            String family = scanner.nextLine();
-            if (family.isEmpty()) {
-                break;
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Map<String,String> addresses = new HashMap<String, String>();
+        while (true)
+        {
+            String city = reader.readLine();
+            if(city.isEmpty()) break;
+            String name = reader.readLine();
+            if (!city.isEmpty() & !name.isEmpty())
+            {
+                addresses.put(city, name);
             }
-
-            list.add(family);
+            else break;
         }
+        String tempCity = reader.readLine();
+        for (Map.Entry<String, String> pair: addresses.entrySet())
+        {
+            String city = pair.getKey();
 
-        // Read the house number
-        int houseNumber = scanner.nextInt();
-
-        if (0 <= houseNumber && houseNumber < list.size()) {
-            String familyName = list.get(houseNumber);
-            System.out.println(familyName);
+            if (tempCity.equals(city))
+            {
+                String name = pair.getValue();
+                System.out.println(name);
+            }
         }
     }
 }
