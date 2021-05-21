a89062796311c1f14d62bc4bb4ab4ffba42c64eb
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-05-21 11:20:11 +0300

    Update Main.java

--- task_4.6/Main.java
+++ task_4.6/Main.java
@@ -9,44 +9,71 @@
 
 public class Main {
     public static void main(String[] args) {
-        Person valera=new Person("Валера","Иванов",69,null,null);
+        Person valera = new Person("Валера","Иванов",69,null,null);
         Person olga = new Person("Ольга","Иванова",67,null,null);
         Person oleg = new Person("Олег","Петров",65,null,null);
         Person jula = new Person("Юля","Петрова",67,null,null);
         Person alex = new Person("Алексей","Иванов",37,olga,valera);
         Person eva  = new Person("Ева","Иванова",37,jula,oleg);
         Person gosha= new Person("Гоша","Иванов",10,eva,alex);
-        System.out.println("Мама Алексея: "+alex.getMother().getName());
-        System.out.println("Бабушка Гоши: "+gosha.getMother().getMother().getName());
+        gosha.getInfo();
 
     }
 }
 
-class Person{
+class Person {
     private String name;
     private String lastname;
     private int age;
     private int hp = 100;
     private Person mother;
     private Person father;
-    public Person(String name, String lastname, int age, Person mother, Person father){
+
+    public Person(String name, String lastname, int age, Person mother, Person father) {
         this.name = name;
         this.lastname = lastname;
         this.age = age;
         this.mother = mother;
         this.father = father;
     }
+
+    public void getInfo() {
+        String data = "Меня зовут " + this.name + "\n";
+        if (this.mother != null) {
+            data += "Имя моей мамы: " + this.mother.name + "\n";
+        }
+        if (this.father != null) {
+            data += "Имя моего папы: " + this.father.name + "\n";
+        }
+        if (this.mother.mother != null) {
+            data += "У меня две бабушки: бабушка " + this.mother.mother.name + " и ";
+        }
+        if (this.father.mother != null) {
+            data += this.father.mother.name + "\n";
+        }
+        if (this.father.father != null) {
+            data += "У меня два дедушки: дедушка " + this.father.father.name + " и ";
+        }
+        if (this.mother.father != null) {
+            data += this.mother.father.name + "\n";
+        }
+        System.out.println(data);
+    }
+
     public String getName() {
         return this.name;
     }
-    public int getHp(){
+
+    public int getHp() {
         return this.hp;
     }
-    public Person getMother(){
+
+    public Person getMother() {
         return this.mother;
     }
-    public void setHp(int hp){
-        if (this.hp+hp>100) this.hp = 100;
+
+    public void setHp(int hp) {
+        if (this.hp + hp > 100) this.hp = 100;
         else this.hp = this.hp + hp;
     }
 }
