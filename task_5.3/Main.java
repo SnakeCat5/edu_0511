70d175603cf242052ed54f2048a070d66acba660
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-05-21 11:56:59 +0300

    Update Main.java

--- task_5.3/Main.java
+++ task_5.3/Main.java
@@ -2,10 +2,19 @@
 * Задание: Дана коллекция с числами. Запишите в новую коллекцию только те числа, которые больше нуля и меньше 10-ти.
 * Коллекции вы создаёте сами
 */
-
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-    
+        ArrayList<Integer> целыечисла = new ArrayList<>();
+        целыечисла.add(22);
+        целыечисла.add(13);
+        целыечисла.add(5);
+        целыечисла.add(18);
+        целыечисла.add(54);
+        целыечисла.add(2);
+        целыечисла.add(8);
+        System.out.println(целыечисла);
+        целыечисла.removeIf(i -> (i > 10));
+        System.out.println(целыечисла);
     }
 }
