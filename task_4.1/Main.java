4d22ec9d53c6be4c9a506abffb8cbdf69df79955
Сергей <82587798+SnakeCat5@users.noreply.github.com>
2021-05-21 09:45:06 +0300

    Update Main.java

--- task_4.1/Main.java
+++ task_4.1/Main.java
@@ -1,17 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat();
-        cat.setName("Жужик");
-        System.out.println(cat.name);
+        cat.setName("Безымянный кот");
+        System.out.println(cat.setName("Имя кота"));
     }
 }
 
 class Cat {
     private String name = "безымянный кот";
 
-    public String setName(String name) {
-        this.name = setName(name);
-
-        return name;
+    public String setName(String name){
+        return (this.name = "Барсик");
+    }
+    public boolean getName(){
+        return false;
     }
 }
